8fb91e58a394f3c6c496c685ef832a7519292163
Ch <carlo.martinucci@gmail.com>
2017-07-25 23:44:59 +0200

    purge

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,23 +1,23 @@
-class ThemesController < Backend::BaseController
-  # POST /themes
-  # POST /themes.json
-  def create
-    @theme = Theme.new(theme_params)
+# class ThemesController < ApplicationController
+#   # POST /themes
+#   # POST /themes.json
+#   def create
+#     @theme = Theme.new(theme_params)
 
-    respond_to do |format|
-      if @theme.save
-        format.html { redirect_to request.referer, notice: 'Tema creato con successo.' }
-        format.json { render :show, status: :created, location: @theme }
-      else
-        format.html { render :new }
-        format.json { render json: @theme.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+#     respond_to do |format|
+#       if @theme.save
+#         format.html { redirect_to request.referer, notice: 'Tema creato con successo.' }
+#         format.json { render :show, status: :created, location: @theme }
+#       else
+#         format.html { render :new }
+#         format.json { render json: @theme.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
 
-  private
-    def theme_params
-      params.require(:theme).permit(:name)
-    end
+#   private
+#     def theme_params
+#       params.require(:theme).permit(:name)
+#     end
 
-end
+# end
